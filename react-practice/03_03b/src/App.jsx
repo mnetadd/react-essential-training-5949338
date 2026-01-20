bad25bf06b1c5c425d93f6a1d622e94a11838564
mnetadd <m.netadd@gmail.com>
2026-01-20 17:19:48 +0100

    03_03b complete

---
+++ react-practice/03_03b/src/App.jsx
@@ -0,0 +1,52 @@
+import "./App.css";
+import chef from "./images/chef.jpg";
+
+function Header({ name, year }) {
+  return (
+    <header>
+      <h1>{name}'s Kitchen</h1>
+      <p>Copyright {year}</p>
+    </header>
+  );
+}
+const items = [
+  "Macaroni and Cheese",
+  "Salmon with Potatoes",
+  "Tofu with Vegetables",
+  "Minestrone Soup"
+];
+
+const dishObjects = items.map((dish, i) => ({
+  id: i,
+  title: dish
+}));
+
+function Main({ dishes }) {
+  return (
+    <main>
+    <img 
+      src={chef} 
+      style={{ width: "200px", height: "300px" }} 
+      alt="Photo of smiling chef" 
+    />
+    <ul>
+      {dishes.map((dish) => (
+        <li key={dish.id} style={{ listStyleType: "none" }}>
+          {dish.title}
+        </li>
+      ))}
+    </ul>
+    </main>
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <Header name="Alex" year={new Date().getFullYear()} />
+      <Main dishes={dishObjects} />
+    </div>
+  );
+}
+
+export default App;
