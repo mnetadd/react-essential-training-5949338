24e293e19dbcd9b33efd740b82e35431d1cc95d2
mnetadd <m.netadd@gmail.com>
2026-01-20 15:36:32 +0100

    changes done per 02_03b chapter

---
+++ react-practice/02_03b/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+let language = "JavaScript"
+let moon = "🌙"
+let emoji = "💖"
+
+function App() {
+  return (
+    <h1>Hello my Vite + React! how is it going in 02_03b with {language} {emoji} {moon}</h1>
+  )
+}
+
+export default App
