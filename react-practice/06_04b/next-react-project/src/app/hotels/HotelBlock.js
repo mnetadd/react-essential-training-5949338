5f04862dc93cc30c9da7b25bec6eaa59c54bb73b
mnetadd <m.netadd@gmail.com>
2026-01-21 00:27:16 +0100

    06_04b complete

---
+++ react-practice/06_04b/next-react-project/src/app/hotels/HotelBlock.js
@@ -0,0 +1,21 @@
+"use client";
+import Image from "next/image";
+
+export default function HotelBlock({ id, name, capacity }) {
+  const imageLoader = ({ src }) => {  
+    return `./hotels/${src}.jpeg`;
+  };
+  return (
+    <div>
+      <h2>Hotel name: {name}, Capacity: {capacity}</h2>
+      <Image
+        src={id} 
+        alt="image file"
+        width={300}
+        height={300}
+        className="w-auto h-auto"
+        loader={imageLoader}
+      />  
+    </div>
+  );
+}
