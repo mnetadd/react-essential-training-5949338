623284286cead836a5164dc49624912dae9d80c6
mnetadd <m.netadd@gmail.com>
2026-01-20 17:42:15 +0100

    03_04b complete

---
+++ react-practice/03_04b/src/App.jsx
@@ -0,0 +1,63 @@
+import "./App.css";
+import chef from "./images/chef.jpg";
+
+function Header({ name, year }) {
+  return (
+    <header>
+      <h1>{name}'s Kitchen</h1>
+      <p>Copyright {year}</p>
+    </header>
+  );
+}
+/* 
+cd .. && mkdir -p 04_01b && cd 04_01b && git fetch upstream && git checkout -b 04_01b && npm crea≈te vite@latest . -- --template react
+*/
+const items = [
+  "Macaroni and Cheese",
+  "Salmon with Potatoes",
+  "Tofu with Vegetables",
+  "Minestrone Soup"
+];
+
+const dishObjects = items.map((dish, i) => ({
+  id: i,
+  title: dish
+}));
+
+function Main({ dishes }) {
+  return (
+    <>
+      <div>
+        <h2>Today's Menu</h2>
+      </div>
+      <main>
+        <img
+          src={chef}
+          height={200}
+          alt="A photo of a smiling chef owner"
+        />
+        <ul>
+          {dishes.map((dish) => (
+            <li
+              key={dish.id}
+              style={{ listStyleType: "none" }}
+            >
+              {dish.title}
+            </li>
+          ))}
+        </ul>
+      </main>
+    </>
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <Header name="Alex" year={new Date().getFullYear()} />
+      <Main dishes={dishObjects} />
+    </div>
+  );
+}
+
+export default App;
